cd6e0f8b586e84944db20a507d8e550ef40e4390
Mykola <46628437+mudmykola@users.noreply.github.com>
2022-06-18 21:26:39 +0300

    fix=>mavigation

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -4,10 +4,10 @@
       <h1 class="uppercase text-100 font-bold text-2xl">multiplex</h1>
     </div>
     <div class="text-200 flex items-center gap-10">
-      <a class="font-medium text-lg hover:text-hoverBtn" href="#home">Головна</a>
-      <a class="font-medium text-lg hover:text-hoverBtn" href="#soon">Скоро</a>
-      <a class="font-medium text-lg hover:text-hoverBtn" href="#about">Про нас</a>
-      <a class="font-medium text-lg hover:text-hoverBtn" href="#popular">Популярне</a>
+      <a class="font-medium text-lg hover:text-hoverBtn transition-all ease-out" href="#home">Головна</a>
+      <a class="font-medium text-lg hover:text-hoverBtn transition-all ease-out" href="#soon">Скоро</a>
+      <a class="font-medium text-lg hover:text-hoverBtn transition-all ease-out" href="#about">Про нас</a>
+      <a class="font-medium text-lg hover:text-hoverBtn transition-all ease-out" href="#popular">Популярне</a>
       <button class="px-8 py-3 bg-100 rounded-full font-bold uppercase text-lg hover:bg-hoverBtn">Купити</button>
     </div>
   </nav>
